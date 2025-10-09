035c3c1037968a4455d049926acb12afbf022572
Christian Roar Weber <christian.roar.weber@gmail.com>
2025-10-09 08:58:21 +0200

    Added test2.c

---
+++ test2.c
@@ -0,0 +1,85 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <assert.h>
+
+#include "aq.h"
+
+#include "aux.h"
+
+/** 
+ * Test with two competing producers?
+ * Make test target using the following command:
+ * make test TEST_FILE=test1.c 
+ */
+
+static AlarmQueue q;
+
+void * producer1 (void * arg) {
+  put_alarm(q, 1);
+  put_normal(q, 2);
+  
+  return 0;
+}
+
+void * producer2 (void * arg) {
+  msleep(250);
+  put_alarm(q, 3);
+  put_normal(q, 4);
+  msleep(500);
+  put_alarm(q, 5);
+  put_normal(q, 6);
+  return 0;
+}
+
+void * consumer(void * arg) {
+  msleep(500);
+  assert (get(q) == 1);
+  assert (get(q) == 2);
+  msleep(500);
+  assert (get(q) == 5);
+  assert (get(q) == 4);
+  assert (get(q) == 6);
+  return 0;
+}
+
+int main(int argc, char ** argv) {
+    int ret;
+
+  q = aq_create();
+
+  if (q == NULL) {
+    printf("Alarm queue could not be created\n");
+    exit(1);
+  }
+  
+  pthread_t t1;
+  pthread_t t2;
+  pthread_t t3;
+
+  void * res1;
+  void * res2;
+  void * res3;
+
+  printf("----------------\n");
+
+  /* Fork threads */
+  pthread_create(&t1, NULL, producer1, NULL);
+  pthread_create(&t2, NULL, producer2, NULL);
+  pthread_create(&t3, NULL, consumer, NULL);
+
+  /* Join with all threads */
+  pthread_join(t1, &res1);
+  pthread_join(t2, &res2);
+  pthread_join(t3, &res3);
+
+  printf("----------------\n");
+  printf("Threads terminated with %ld, %ld, %ld\n", (uintptr_t) res1, (uintptr_t) res2, (uintptr_t) res3);
+
+  print_sizes(q);
+  
+  return 0;
+}
+
